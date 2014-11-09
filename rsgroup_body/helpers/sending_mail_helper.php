a1dfeb546732d6c945cbff038c8f31bef076e3cd
ranasoyab <ranasoyab@gmail.com>
2014-11-09 17:53:44 +0530

    mail setting

--- rsgroup_body/helpers/sending_mail_helper.php
+++ rsgroup_body/helpers/sending_mail_helper.php
@@ -5,17 +5,20 @@ if (!function_exists('send_mail')) {
     function send_mail($tomailid, $subject, $message, $cc = '', $attachement = '') {
 
         $ci = get_instance();
+        
+        $mail_setting = Setting::getSetting('smtp');
+
         $config = Array(
             'protocol' => 'smtp',
-            'smtp_port' => 587,
-            'smtp_host' => 'mail.rootitsolutions.com',
-            'smtp_user' => 'info@rootitsolutions.com',
-            'smtp_pass' => 'rsgroup@2014',
+            'smtp_port' => $mail_setting['setting']['smtp_port'],
+            'smtp_host' => $mail_setting['setting']['smtp_host'],
+            'smtp_user' => $mail_setting['setting']['smtp_mail'],
+            'smtp_pass' => $mail_setting['setting']['smtp_password'],
             'mailtype' => 'html',
         );
         $ci->load->library('email', $config);
         $ci->email->set_newline("\r\n");
-        $ci->email->from('info@rootitsolutions.com', 'Root IT Solutions');
+        $ci->email->from('info@rootitsolutions.com', $mail_setting['setting']['smtp_name']);
         $ci->email->to($tomailid);
         $ci->email->subject($subject);
         $ci->email->message($message);
--- rsgroup_body/models/setting.php
+++ rsgroup_body/models/setting.php
@@ -5,6 +5,26 @@ class Setting extends DataMapper
     function __construct($id = NULL) {
         parent::__construct($id);
     }
+
+
+    public static function getSetting($type){
+    	$ci = get_instance();
+        $ci->db->_protect_identifiers = false;
+        $ci->db->select('*');
+        $ci->db->from('settings');
+        $ci->db->where('system_type', $type);
+        $res = $ci->db->get();
+        if ($res->num_rows > 0) {
+            $setting = $res->result();
+            foreach ($setting as $value) {
+	            $return['setting'][$value->system_key] = $value->system_value;
+	        }
+
+	        return $return;
+        } else {
+            return false;
+        }
+    }
     
 }
 ?>
